754797ef5756f463d3c9dc30a4ecc28837f26eb4
Kyle Horton <kylehorton33@gmail.com>
2023-11-19 16:01:23 -0500

    feat: :truck: new 'echo' route to test deployment from another machine

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+const { application } = require('express');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.json({ debug: "Kyle's Personal API -  2023.11.19" })
+})
+
+app.get('/echo', (req, res) => {
+    res.json({ content: req.headers})
 })
 
 app.listen(port, () => {
